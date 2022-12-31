db41be61fae65d5bc2dbafb586e340182febe93e
Abhiram157 <105275404+Abhiram157@users.noreply.github.com>
2022-12-31 16:32:02 +0530

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -5,7 +5,7 @@ import main
 
 class Test_2(unittest.TestCase):
   def test_02(self):
-    self.assertEqual(main.highest_of(6,94), 94)
+    self.assertEqual(main.digits_of(186), 3)
     
 if __name__ == '__main__':
   unittest.main(verbosity=2)
