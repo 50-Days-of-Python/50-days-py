47d589ec52292481c55eb2dbb4f930de992f347d
Abhiram157 <105275404+Abhiram157@users.noreply.github.com>
2022-12-31 16:08:45 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -6,9 +6,6 @@ import main
 class Test_1(unittest.TestCase):
   def test_01(self):
     self.assertEqual(main.highest_of(5,4), 5)
-
-  def test_02(self):
-    self.assertEqual(main.highest_of(6,94), 94)
     
 if __name__ == '__main__':
   unittest.main(verbosity=2)
